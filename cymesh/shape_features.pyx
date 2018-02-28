2f1b0f307de898efc21dce9cf86782809a3c6c14
Joel Simon <joelsimon6@gmail.com>
2018-02-27 19:57:50 -0500

    pass random seed to shape feature

--- cymesh/shape_features.pyx
+++ cymesh/shape_features.pyx
@@ -16,11 +16,13 @@ from cymesh.structures cimport Vert
 ctypedef double (*vert_metric)(Vert, Vert)
 
 cdef double[:] create_features(vert_metric metric, Mesh mesh, tuple hrange, \
-                               int n_points, int n_bins) except *:
+                               int n_points, int n_bins, int rseed) except *:
     cdef int i, j, n, n_verts
     cdef double mean_value, rms, hmin, hmax
     cdef double[:] values = np.zeros(n_points)
     cdef double values_sqr = 0
+
+    srand(rseed)
 
     if hrange is not None:
         hmin = hrange[0]
@@ -53,15 +55,17 @@ cdef double d2_metric(Vert a, Vert b):
 cdef double a2_metric(Vert a, Vert b):
     return vangle(a.normal, b.normal)
 
-cpdef double[:] d2_features(Mesh mesh, tuple hrange=(0.0, 3.0), int n_points=1024, int n_bins=64) except *:
+cpdef double[:] d2_features(Mesh mesh, tuple hrange=(0.0, 3.0), int n_points=1024,
+                            int n_bins=64, int rseed=123) except *:
     """ 3D Shape features from 'Matching 3D Models with Shape Distributions'
         Paper pdf: https://graphics.stanford.edu/courses/cs468-01-fall/Papers/osada_funkhouser_chazelle_dobkin.pdf
         Returns a feature vector of size 'bins' from arbitrary sized mesh.
     """
-    return create_features(d2_metric, mesh, hrange, n_points, n_bins)
+    return create_features(d2_metric, mesh, hrange, n_points, n_bins, rseed)
 
-cpdef double[:] a2_features(Mesh mesh, tuple hrange=(0.0, 3.0), int n_points=1024, int n_bins=64) except *:
-    """ Similar to d2 features but used the angle between the normal direction.
+cpdef double[:] a2_features(Mesh mesh, tuple hrange=(0.0, 3.0), int n_points=1024,
+                            int n_bins=64, int rseed=123) except *:
+    """ Similar to d2 features but use the angle between the normal directions.
     """
     mesh.calculateNormals()
-    return create_features(a2_metric, mesh, hrange, n_points, n_bins)
+    return create_features(a2_metric, mesh, hrange, n_points, n_bins, rseed)
